3c573f75385874cf84c8126dc34c7f01516e0b1d
Alexandr-K1 <alexsa1995@gmail.com>
2024-09-08 20:10:00 +0300

    Second commit

--- HW_M3_Task_2.py
+++ HW_M3_Task_2.py
@@ -3,25 +3,30 @@
 
 from random import randint
 
-#Просимо користувача заповнити параметри
-min = int(input('Введіть значення min: '))
-max = int(input('Введіть значення max: '))
-quantity = int(input('Введіть значення quantity: '))
-
-#Створюємо пусти множину
-result = set()
-
 #Створюємо функцію для генерації унікальних випадкових чисел
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 and max > 1000:  
-        print('Значення min повинно бути не менше за 1, а max - не більше за 1000!')
+
+    #Створюємо пусту множину
+    result = set()
+
+    if min < 1 and max > 1000 and quantity >= (max-min): 
+        return 'Значення min повинно бути не менше за 1, max - не більше за 1000, quantity - не більше за різницю між max та min!'
+    elif min < 1 and max > 1000:
+        return 'Значення min повинно бути не менше за 1, а max - не більше за 1000!'  
+    elif min < 1 and quantity >= (max-min):  
+        return 'Значення min повинно бути не менше за 1, а quantity - не більше за різницю між max та min!'   
+    elif max > 1000 and quantity >= (max-min):  
+        return 'Значення max повинно бути не більше за 1000, а quantity - не більше за різницю між max та min!'
     elif max > 1000:
-        print('Значення max повинно бути не більше за 1000!')
+        return 'Значення max повинно бути не більше за 1000!'
     elif min < 1:
-        print('Значення min повинно бути не менше за 1!')  
+        return 'Значення min повинно бути не менше за 1!'
+    elif max < min:
+        return 'Значення max повинно бути більше за min!'        
+    elif quantity >= (max-min):
+        return 'Значення quantity повинно бути не більше за різницю між max та min!'             
     else:
         while len(result) != quantity:            
-            result.add(randint(min, max))  
-        print(sorted(list(result)))  
-  
-get_numbers_ticket(min, max, quantity)
+            result.add(randint(min, max))
+
+    return sorted(result)
