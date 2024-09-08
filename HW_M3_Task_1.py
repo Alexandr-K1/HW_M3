903609e7b38d7f14c0f268f9c8cc61983567a9a3
Alexandr-K1 <alexsa1995@gmail.com>
2024-09-08 19:47:55 +0300

    Third commit

--- HW_M3_Task_1.py
+++ HW_M3_Task_1.py
@@ -9,8 +9,7 @@ def get_days_from_today(date):
         #Отримуємо поточну дату   
         today_date = datetime.today().date()        
         #Розраховуємо різницю у днях 
-        delta_days = today_date - converted_date
-        #print(f'Кількість днів між заданою датою і поточною датою = {delta_days.days}')  
+        delta_days = today_date - converted_date        
     except ValueError:
         return 'Error! String not format YYYY.MM.DD!'      
     except UnboundLocalError:
